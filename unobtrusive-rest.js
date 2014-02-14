b2821e61852f4d7cb1460b950b2b88e15bb1f750
Jim Wu <jwu.work@gmail.com>
2014-02-14 13:54:12 -0800

    Add sample file and fix spacing

--- unobtrusive-rest.js
+++ unobtrusive-rest.js
@@ -1,22 +1,22 @@
 // Handles "data-method" on links such as:
 // <a href="/logout" data-method="post">Sign Out</a>
 function handleMethod(link) {
-    var href = link.attr('href');
-    var method = link.data('method');
-    var form = $('<form method="' + method + '" action="' + href + '"></form>');
-	
+	var href = link.attr('href');
+	var method = link.data('method');
+	var form = $('<form method="' + method + '" action="' + href + '"></form>');
+
 	// TODO: update the following line to select your antiforgery token.
-    var csrfToken = $('input[name=__RequestVerificationToken]');
-	
-    form.hide().append(csrfToken).appendTo('body');
-    form.submit();
+	var csrfToken = $('input[name=__RequestVerificationToken]');
+
+	form.hide().append(csrfToken).appendTo('body');
+	form.submit();
 };
 
 $(document).delegate('a[data-method]', 'click', function (e) {
-    var link = $(this);
+	var link = $(this);
 
-    if (link.data('method')) {
-        handleMethod(link);
-        return false;
-    }
+	if (link.data('method')) {
+		handleMethod(link);
+		return false;
+	}
 });
