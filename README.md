c466b7c47202c5d2b181550720edb1e61221929d
Jim Wu <jwu.work@gmail.com>
2014-02-14 13:42:16 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Handle form submissions unobtrusively.
+
+[Rails Unobtrusive JavaScript](https://github.com/rails/jquery-ujs/blob/master/src/rails.js)
---
+++ unobtrusive-rest.js
@@ -0,0 +1,22 @@
+// Handles "data-method" on links such as:
+// <a href="/logout" data-method="post">Sign Out</a>
+function handleMethod(link) {
+    var href = link.attr('href');
+    var method = link.data('method');
+    var form = $('<form method="' + method + '" action="' + href + '"></form>');
+	
+	// TODO: update the following line to select your antiforgery token.
+    var csrfToken = $('input[name=__RequestVerificationToken]');
+	
+    form.hide().append(csrfToken).appendTo('body');
+    form.submit();
+};
+
+$(document).delegate('a[data-method]', 'click', function (e) {
+    var link = $(this);
+
+    if (link.data('method')) {
+        handleMethod(link);
+        return false;
+    }
+});
